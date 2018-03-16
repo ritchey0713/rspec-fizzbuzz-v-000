c3f2fb166c7b0595ce4f62cafb0ec7700108b5e7
David Ritchey <ritchey0713@gmail.com>
2018-03-16 21:47:43 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,8 +3,8 @@ def fizzbuzz(value)
     return "Fizz"
     elsif value % 5 == 0 
     return "Buzz"
-    elsif value % 15 == 0 
-    return "FizzBuzz"
+    # elsif value % 15 == 0 
+    # return "FizzBuzz"
   else
     return nil;
   end
